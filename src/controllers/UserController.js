f8cfb477106967257dfc02296ed6a9a5a369850d
Andr3sdosSantos <andres_santosbritoamaral@hotmail.com>
2020-05-25 17:25:54 -0300

    C.R.U.D with KnexJS

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -5,5 +5,48 @@ module.exports = {
     const users = await knex('users').then();
 
     return res.json(users);
-  }
+  },
+
+  async store(req, res, next) {
+    try {
+      const { username } = req.body;
+
+      await knex('users').insert({
+        username,
+      });
+
+      return res.status(200).send();
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  async update(req, res, next) {
+    try {
+      const { id } = req.params;
+      const { username } = req.body;
+
+      await knex('users')
+        .update({ username })
+        .where({ id });
+
+      return res.send();
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  async delete(req, res, next) {
+    const { id } = req.params;
+
+    try {
+      await knex('users')
+        .where({ id })
+        .delete();
+
+      return res.send(); 
+    } catch (error) {
+      next(error);
+    }
+  },
 }
