ac423d5d35d08538ee474caee3386ebb44277452
Andr3sdosSantos <andres_santosbritoamaral@hotmail.com>
2020-05-25 17:27:51 -0300

    Server of aplication

--- src/server.js
+++ src/server.js
@@ -3,7 +3,17 @@ const routes = require('./routes');
 
 const app = express();
 
+app.use(express.json())
 app.use(routes);
+
+/**
+ * Tentativa de retornar o nome do error por padrão.
+ */
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({ error: error.message });
+
+});
 
 app.listen(3333, () => {
   console.log(`Server is runing...`);
