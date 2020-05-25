20bed0c2bcc221c6fd3d3bb2587c2bfbc07298a3
Andr3sdosSantos <andres_santosbritoamaral@hotmail.com>
2020-05-25 17:31:57 -0300

    Knexfile

--- knexfile.js
+++ knexfile.js
@@ -3,42 +3,21 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
+    client: 'pg',
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+      user: 'postgres',
+      host: '127.0.0.1',
+      database: 'knexjs',
+      password: 'docker',
+      port: '5432'
+    }, 
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tableName: 'knex_migrations',
+      directory: `${__dirname}/src/database/migrations`,
+    },
+    seeds: {
+      directory: `${__dirname}/src/database/seeds`,
+    },
   },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
 
 };
